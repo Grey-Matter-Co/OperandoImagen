7b90d9d382424a7882fbff6b7ed3beaa2b246c13
M1ndBlast <gustavopdzz0@gmail.com>
2020-04-02 16:17:12 -0600

    implement function to grey scale

--- readingFile.c
+++ readingFile.c
@@ -2,7 +2,7 @@
 #include "stdlib.h"
 #include "string.h"
 
-// inclir librerias para trabajar con imagenes
+// inclir librerias para trabajar con imagenes, estas declaraciones son obligaroeias (segun el desarrollador de las librerias)
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb/stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
@@ -13,10 +13,11 @@ void main()
 	// inicializacion de parametros
 	int width, height, channels;	
 	char path[260];	// 260 porque es la ruta maxima en windows
+	unsigned char *img;
 
-	while (1)
+	while (1)	// Ciclo para validar si el programa pudo cargar la imagen
 	{
-		while (1)
+		while (1)	// Ciclo para validar si la ruta del archivo es valida
 		{
 			printf("ruta del archivo (relativa | absoluta): ");
 			scanf("%s", &path);
@@ -30,7 +31,7 @@ void main()
 		}
 		
 		// lectura de la imagen
-		unsigned char *img = stbi_load(path, &width, &height, &channels, 0);
+		img = stbi_load(path, &width, &height, &channels, 0);
 
 		// comprobar si la lectura es exitosa
 		if (img == NULL) 
@@ -44,4 +45,25 @@ void main()
 
 	// comprobacion de imagen
 	printf("Loaded image with a width of %dpx, a height of %dpx and %d channels\n", width, height, channels);
+
+	// Convert the input image to gray
+	size_t img_size = width * height * channels;
+ 	int gray_channels = channels == 4 ? 2 : 1;
+	size_t gray_img_size = width * height * gray_channels;
+
+	unsigned char *gray_img = malloc(gray_img_size);
+	if(gray_img == NULL) 
+	{
+		printf("Unable to allocate memory for the gray image.\n");
+		exit(1);
+	}
+
+	for(unsigned char *p = img, *pg = gray_img; p != img + img_size; p += channels, pg += gray_channels) 
+	{
+		*pg = (uint8_t)((*p + *(p + 1) + *(p + 2))/3.0);
+		if(channels == 4)
+			*(pg + 1) = *(p + 3);
+	}
+
+	stbi_write_jpg("test.jpg", width, height, gray_channels, gray_img, 100);
 }
